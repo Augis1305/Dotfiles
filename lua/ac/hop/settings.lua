dfee2d999f6a1183e5f744a996a134f57227e515
AugustinasCerniauskas <acerniauskas@opus2.com>
2022-10-24 09:28:55 +0100

    fix

--- lua/ac/hop/settings.lua
+++ lua/ac/hop/settings.lua
@@ -2,3 +2,4 @@ require'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
 
 nnoremap('<leader>hw', '<cmd>HopWord<cr>')
 nnoremap('<leader>hl', '<cmd>HopLine<cr>')
+nnoremap('<leader>hc', '<cmd>HopChar1<cr>')
