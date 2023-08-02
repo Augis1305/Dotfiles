21a3f1977359aebcbfa14968fccf9eb528fc2256
AugustinasCerniauskas <acerniauskas@opus@.com>
2023-08-02 10:42:57 +0100

    test

--- lua/plugins/todo.lua
+++ lua/plugins/todo.lua
@@ -4,6 +4,7 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     -- TODO test
     opts = {
+
       -- your configuration comes here
       -- or leave it empty to use the default settings
       -- refer to the configuration section below
