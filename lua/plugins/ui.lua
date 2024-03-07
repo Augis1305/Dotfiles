9b16a63a23bc65729b7f843edb96432bfbc5114a
AugustinasCerniauskas <acerniauskas@opus@.com>
2024-03-07 07:49:46 +0000

    feat: added zen mode

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -22,4 +22,16 @@ return {
     event = "BufReadPost",
     config = true,
   },
+  {
+    "folke/zen-mode.nvim",
+    cmd = "ZenMode",
+    opts = {
+      plugins = {
+        gitsigns = true,
+        tmux = true,
+        kitty = { enabled = false, font = "+2" },
+      },
+    },
+    keys = { { "<leader>z", "<cmd>ZenMode<cr>", desc = "Zen Mode" } },
+  },
 }
